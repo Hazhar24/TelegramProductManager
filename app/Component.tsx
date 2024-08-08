bbb1c8b856806b62eb6c2063eb3dd45d90fc4069
hazhar24 <hazhar24ghaderi@gmail.com>
2024-08-08 11:43:59 +0330

    add component input

---
+++ app/Component.tsx
@@ -0,0 +1,10 @@
+const Input1 = () => {
+    return (
+        <div className="flex flex-col">
+            <label className="py-3" htmlFor="input1">Supplier</label>
+            <input className="text-black rounded-md w-72 outline-none h-12 px-5 " type="text" id="input1" placeholder="Input Product ..." />
+        </div>
+    );
+}
+
+export default Input1
